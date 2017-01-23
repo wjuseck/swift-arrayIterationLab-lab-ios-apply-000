b599ae7346d96e8340c09c07cb8175940a99064d
JimCampagno <jimcampagno@gmail.com>
2017-01-23 14:14:26 -0500

    Create solution

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -8,33 +8,33 @@
  * [Array](https://github.com/learn-co-curriculum/swift-array-readme)
  * [Array Iteration](https://github.com/learn-co-curriculum/swift-arrayIteration-readme)
  
-
+ 
  */
 /*: question1
- ### 1. You're building an app and want to store all of the ingredients added to a cart. How would you represent this cart in code? The ingredients are "Chips", "Salsa", "Guacamole", "Red wine". Explicitly mark the content of the cart as strings.
+ ### 1. You're building an app and want to store all of the ingredients added to a cart into an `Array`. The ingredients are "Chips", "Salsa", "Guacamole", and "Red wine". Name this `Array` variable whatever you like, think of a name which makes sense.
  */
-// write your code here
 
-
+var ingredients = ["Chips", "Salsa", "Guacamole", "Red Wine"]
 
 
 
 
 /*: question2
- ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code? Explicitly mark the content of the cart as numbers.
+ ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code?
  */
-// write your code here
-
-
-
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 
 
 /*: question3
- ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible.
+ ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible. (Hint: for-in loop).
  */
-// write your code here
+for number in numbers {
+    
+    print(number)
+    
+}
 
 
 
@@ -43,10 +43,13 @@
 
 
 /*: question4
- ### 4. Take the list of shopping cart items you created in question 1 and print their values in the most efficient way possible. Prefix each item in the array with a string of text.
+ ### 4. Take the list of shopping cart items you created in question 1 and iterate through it printing each item to the console.
  */
-// write your code here
-
+for ingredient in ingredients {
+    
+    print(ingredient)
+    
+}
 
 
 
@@ -54,31 +57,44 @@
 
 
 /*: question5
- ### 5. Take the list of shopping cart items you created in question 1 and print their values by passing each item in list to a function to be printed.
+ ### 5. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
  */
-// write your code here
-
-
+func greet(names: [String]) {
+    
+    for name in names {
+        
+        if name == "Michael" {
+            print("Top of the morning Michael!")
+        } else {
+            print("Good morning \(name).")
+        }
+        
+    }
+    
+}
 
 
 
 
 
 /*: question6
- ### 4. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
+ ### 6. Create a function that takes an array of Ints and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
-// write your code here
-
-
-
-
-
-
-/*: question7
- ### 7. Create a function that takes an array of Ints (40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49) and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
- */
-// write your code here
-
+func lessThan50(numbers: [Int]) -> [Int] {
+    
+    var result: [Int] = []
+    
+    for number in numbers {
+        
+        if number < 50 {
+            result.append(number)
+        }
+        
+    }
+    
+    return result
+    
+}
 
 
 
