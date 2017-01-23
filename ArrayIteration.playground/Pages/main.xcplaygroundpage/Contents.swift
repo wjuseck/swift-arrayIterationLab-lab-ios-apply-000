d9eb7a051444ec4171d56dcd1d236cc7afc9422f
JimCampagno <jimcampagno@gmail.com>
2017-01-23 14:04:58 -0500

    Add changes to Playground

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -11,7 +11,7 @@
 
  */
 /*: question1
- ### 1. You're building an app and want to store all of the ingredients added to a cart. How would you represent this cart in code? The ingredients are "Chips", "Salsa", "Guacamole", "Red wine". Explicitly mark the content of the cart as strings.
+ ### 1. You're building an app and want to store all of the ingredients added to a cart into an `Array`. The ingredients are "Chips", "Salsa", "Guacamole", and "Red wine". Name this `Array` variable whatever you like, think of a name which makes sense.
  */
 // write your code here
 
@@ -21,7 +21,7 @@
 
 
 /*: question2
- ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code? Explicitly mark the content of the cart as numbers.
+ ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code?
  */
 // write your code here
 
@@ -32,7 +32,7 @@
 
 
 /*: question3
- ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible.
+ ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible. (Hint: for-in loop).
  */
 // write your code here
 
@@ -43,7 +43,7 @@
 
 
 /*: question4
- ### 4. Take the list of shopping cart items you created in question 1 and print their values in the most efficient way possible. Prefix each item in the array with a string of text.
+ ### 4. Take the list of shopping cart items you created in question 1 and iterate through it printing each item to the console.
  */
 // write your code here
 
@@ -54,10 +54,9 @@
 
 
 /*: question5
- ### 5. Take the list of shopping cart items you created in question 1 and print their values by passing each item in list to a function to be printed.
+ ### 5. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
  */
 // write your code here
-
 
 
 
@@ -65,17 +64,7 @@
 
 
 /*: question6
- ### 4. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
- */
-// write your code here
-
-
-
-
-
-
-/*: question7
- ### 7. Create a function that takes an array of Ints (40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49) and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
+ ### 6. Create a function that takes an array of Ints and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
 // write your code here
 
